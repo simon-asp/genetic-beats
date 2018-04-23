ce767e10414923fb0721b502cd01c27ed3637a55
fgrhlt <simpasp@gmail.com>
2018-04-23 17:39:17 +0200

    update  database for login

--- src/components/BeatList/BeatList.js
+++ src/components/BeatList/BeatList.js
@@ -229,7 +229,7 @@ class BeatList extends React.Component {
 						onClick={this.onFinishClick.bind(this)}
 						colored={true} 
 						hidden={this.state.higherGenerationExists}
-						unRender={this.props.timelineIndex < 3} />
+						unRender={this.props.timelineIndex < 1} />
 					
 					<Tooltip text={this.state.tooltipText} 
 						active={this.state.showTooltip}
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -58,7 +58,7 @@ class Login extends React.Component {
       auth().createUserWithEmailAndPassword(email, password)
       .then(() => {
         // Add user to database
-        database.ref('/users/').push({
+        database.ref('/alpha/').push({
           userId: email
         })
         .catch(e => {
