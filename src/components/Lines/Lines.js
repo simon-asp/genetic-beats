2f417974263451126d47606b6e721bf2805f628c
fgrhlt <simpasp@gmail.com>
2018-04-26 21:49:20 +0200

    Add and render lines works more fluently

--- src/components/Lines/Lines.js
+++ src/components/Lines/Lines.js
@@ -7,7 +7,7 @@ import { mapRange, getCenterCoords } from '../../utils';
 let cx = classNames.bind(s);
 
 class Lines extends React.Component {
-  static propTypes = {
+	static propTypes = {
 		domNodesTimeline: PropTypes.array,
 		beatInfo: PropTypes.object,
 		evolutionPairs: PropTypes.array,
@@ -15,29 +15,35 @@ class Lines extends React.Component {
 	componentWillMount() {
 		this.setState({
 			linesTimeline: [],
-		})
+		});
+
+		this.addLines(this.props).then((props) => {
+			this.renderLines(props);
+		});
 	}
 
-  // Render or unrender the lines depending on Redux props
+		// Render or unrender the lines depending on Redux props
 	componentWillReceiveProps(nextProps) {
 		const { evolutionPairs, domNodesTimeline, timelineLength } = nextProps;
 		const scoreOrLikedChanged = this.scoreOrLikedChanged(nextProps);
-		console.log(scoreOrLikedChanged)
 		/* Add the lines to the dom, on the second iteration and more, then render them.
-		 * Only add if the score or liked state of all beats did not change.
-		 */
-		if(domNodesTimeline.length > 1 && timelineLength > 1 && evolutionPairs.length > 0 && !scoreOrLikedChanged) {
+			* Only add if the score or liked state of all beats did not change.
+			*/
+		if(this.isTimelineLengthLarger(nextProps)) {
 			this.addLines(nextProps).then((props) => {
 				this.renderLines(props);
 			});
 		}
 		
-    // Only unrender lines for the first generation, when we click the reset beats.
+		// Only unrender lines for the first generation, when we click the reset beats.
 		if (evolutionPairs.length === 0 && timelineLength === 1) this.unrenderLines(nextProps);
 	}
 
+	isTimelineLengthLarger(nextProps) {
+		return nextProps.timelineLength > this.props.timelineLength;
+	}
 	// Compares the nextProps to this.props to see if the score or liked changed of the beats
-	scoreOrLikedChanged = (nextProps) => {
+	scoreOrLikedChanged(nextProps) {
 		const { beatTimeline } = nextProps;
 
 		if(beatTimeline.length > this.props.beatTimeline.length) return false;
@@ -56,9 +62,9 @@ class Lines extends React.Component {
 	}
 
 	/* Add lines to the DOM, for every beatlist after the first generation. Every new generation gets
-	 * two lines, that are connected to the previous generation. */
+		* two lines, that are connected to the previous generation. */
 	addLines = (props) => {
-		console.log("AddLines")
+		console.log("AddLines: ", props)
 		return new Promise(resolve => {
 			
 			// Purple, light blue, dark blue, 
@@ -66,13 +72,11 @@ class Lines extends React.Component {
 			const { beatInfo, timelineLength, beatTimeline, evolutionPairs, domNodesTimeline } = props;
 			const linesTimeline = [];
 			const lines = [];
-
 			if (beatInfo) {
-				// Add lines for -1 generation, so the first generation doesn't get lines.
-				for(let index = 0; index < domNodesTimeline.length-1; index++) {
+				// Add lines for generation - 1, so the first generation doesn't get lines.
+				for(let index = 0; index < timelineLength-1; index++) {
 					let beatlist = beatTimeline[index];
 					for (let j = 0; j < beatInfo.noOfBeats; j++) {
-						
 						const p1Index = evolutionPairs[index][j].parent1;
 						const p2Index = evolutionPairs[index][j].parent2;
 						const parentIndices = [p1Index, p2Index];
@@ -105,7 +109,7 @@ class Lines extends React.Component {
 		})
 	};
 
-  // Doesn't work yet. Should add a glow-effect to the lines.
+	// Doesn't work yet. Should add a glow-effect to the lines.
 	addFilter = () => {
 		const svg = document.getElementById('svg');
 		if (svg) {
@@ -126,12 +130,18 @@ class Lines extends React.Component {
 
   /* Reset all lines */
   unrenderLines = (props) => {
-		const { beatInfo, timelineLength } = props;
-    for (let index = 0; index < timelineLength-1; index++) {	
-			for (let i = 0; i < beatInfo.noOfBeats; i++) {
-				document.getElementById('line' + index + '' + i).setAttribute('d', 'M0 0 C 0 0, 0 0, 0 0');
-			}
+	const { beatInfo, timelineLength } = props;
+	const index = timelineLength-1;
+	for (let i = 0; i < beatInfo.noOfBeats; i++) {
+		const line1 = document.getElementById('line' + index + '' + i + 0);
+		const line2 = document.getElementById('line' + index + '' + i + 1);
+		
+		if(!line1) return;
+		else {
+			line1.setAttribute('d', 'M0 0 C 0 0, 0 0, 0 0');
+			line2.setAttribute('d', 'M0 0 C 0 0, 0 0, 0 0');
 		}
+	}
   }
 
 	/* Calculate the x, y-coordinates for the lines and draw them out */
@@ -162,8 +172,6 @@ class Lines extends React.Component {
 				const cP2x = parent2Coord.x - 50;
 				const cP2y = parent2Coord.y + 250;
 				//const sx1 = 1; const sx2 = 1; const sy1 = 1; const sy2 = 1;
-
-				// TODO: RENDER 3 LINES PER THING
 				
 				// Bezier curve coords
 				const d = 'M' + startCoord.x +' '+startCoord.y + ' C ' + cS2x +' '+ cSy + ', ' + cP1x +' '+ cP1y + ', ' + parent1Coord.x +' '+ parent1Coord.y;
@@ -172,8 +180,10 @@ class Lines extends React.Component {
 				let lineDom1 = document.getElementById('line' + index + i + 0);
 				let lineDom2 = document.getElementById('line' + index + i + 1);
 				
-				lineDom1.setAttribute('d', d);
-				lineDom2.setAttribute('d', e);
+				if(lineDom1) {
+					lineDom1.setAttribute('d', d);
+					lineDom2.setAttribute('d', e);
+				}
 
 				const colors = ['#5C429B', '#81DFEF', '#1D2DBF', '#6D0F3A', '#FFFFFF', '#8FDD76', '#F286B1', '#EDDA54'];
 			}
@@ -181,14 +191,14 @@ class Lines extends React.Component {
 	}
 
   render() {
-		// The height of the whole svg. 40vh per beatlist. Should cover all beatlists.
-		const height = (this.props.timelineLength*40) + 'vh';
+	// The height of the whole svg. 40vh per beatlist. Should cover all beatlists.
+	const height = (this.props.timelineLength*40) + 'vh';
     return (
-			<div className={s.root}>
-				<svg id="svg" style={{ height: 'calc('+height+' + 106px)'}}>
-					{ this.state.linesTimeline }
-				</svg>
-			</div>
+		<div className={s.root}>
+			<svg id="svg" style={{ height: 'calc('+height+' + 106px)'}}>
+				{ this.state.linesTimeline }
+			</svg>
+		</div>
     );
   }
 }
