c6eea69537d30ef36b48d04a6878efb35f484076
fgrhlt <simpasp@gmail.com>
2018-04-27 20:36:03 +0200

    refactor rendering of lines

--- src/components/Lines/Lines.js
+++ src/components/Lines/Lines.js
@@ -18,8 +18,8 @@ class Lines extends React.Component {
 		});
 
 		// Initial render of lines
-		this.addLines(this.props).then((props) => {
-			this.renderLines(props);
+		this.addAllLinesOnce(this.props).then((props) => {
+			this.renderAllLines(props);
 		});
 	}
 
@@ -30,7 +30,7 @@ class Lines extends React.Component {
 		 * Only add if we have a new generation of beats */
 		if(this.isTimelineLengthLarger(nextProps)) {
 			this.addLines(nextProps).then((props) => {
-				this.renderLines(props);
+				this.renderLines(props, timelineLength-2);
 			});
 		}
 		
@@ -44,54 +44,84 @@ class Lines extends React.Component {
 	}
 
 	/* Add lines to the DOM, for every beatlist after the first generation. Every new generation gets
-		* two lines, that are connected to the previous generation. */
-	addLines = (props) => {
+	 * two lines, that are connected to the previous generation. */
+	addAllLinesOnce(props) {
+		const { beatInfo, timelineLength, beatTimeline, evolutionPairs, domNodesTimeline } = props;
 		return new Promise(resolve => {
-			
-			// Purple, light blue, dark blue, 
-			const colors = ['#5C429B', '#81DFEF', '#1D2DBF', '#6D0F3A', '#FFFFFF', '#8FDD76', '#F286B1', '#EDDA54'];
-			const { beatInfo, timelineLength, beatTimeline, evolutionPairs, domNodesTimeline } = props;
 			const linesTimeline = [];
-			const lines = [];
-			if (beatInfo) {
-				// Add lines for generation - 1, so the first generation doesn't get lines.
-				for(let index = 0; index < timelineLength-1; index++) {
-					let beatlist = beatTimeline[index];
-					for (let j = 0; j < beatInfo.noOfBeats; j++) {
-						const p1Index = evolutionPairs[index][j].parent1;
-						const p2Index = evolutionPairs[index][j].parent2;
-						const parentIndices = [p1Index, p2Index];
-						const score1 = beatlist[p1Index].score;
-						const score2 = beatlist[p2Index].score;
-
-						const strokeWidth = [mapRange(score1, 1, 5, 1, 5), mapRange(score2, 1, 5, 1, 5)];
-						// Add weak and strong classname to intensify the opacity
-						const lineClass = cx('line', {weak: (score1 + score2) <= 5, strong: (score1 + score2) >= 9});						
-						
-						// Add two lines per domNode
-						for(let lineIndex = 0; lineIndex < 2; lineIndex++) {
-							lines.push(<path
-								id={'line' + index + j + lineIndex}
-								stroke={colors[parentIndices[lineIndex]]}
-								strokeWidth={strokeWidth[lineIndex]}
-								className={lineClass}
-								key={'line' + index + j + lineIndex}
-								fill="transparent"
-							/>);
-						}
-					}
-				}
-				linesTimeline.push(lines);			
+			let lines = [];
+			// Return if we only have one generation
+			if(timelineLength === 1) return;
+			
+			for(let i = 0; i < timelineLength - 1; i++) {
+				lines = this.createLines(props, i);	
+				linesTimeline.push(lines);
 			}
+
 			// Set the lines to state, and resolve the promise
 			this.setState({linesTimeline}, () => {
 				resolve(props);
 			});
 		})
 	};
+	
+	// Add lines only for the newly created generation.
+	addLines(props) {
+		const { beatInfo, timelineLength, beatTimeline, evolutionPairs, domNodesTimeline } = props;
+		return new Promise(resolve => {
+			let linesTimeline = [];
+			let lines = [];
+			this.state ? linesTimeline = this.state.linesTimeline : [];
+			
+			// Since evolution pairs are only created on the second generation
+			const index = timelineLength - 2;
+			lines = this.createLines(props, index);	
+			
+			linesTimeline.push(lines);
+			console.log("addlines one", lines)
+			// Set the lines to state, and resolve the promise
+			this.setState({linesTimeline}, () => {
+				resolve(props);
+			});
+		})
+	}
+
+	// Create lines for every beatlist and return an array of lines
+	createLines(props, index) {
+		const { beatInfo, timelineLength, beatTimeline, evolutionPairs, domNodesTimeline } = props;
+		const colors = ['#5C429B', '#81DFEF', '#1D2DBF', '#6D0F3A', '#FFFFFF', '#8FDD76', '#F286B1', '#EDDA54'];
+		const lines = [];
+
+		// Add lines for generation - 1, so the first generation doesn't get lines.
+		let beatlist = beatTimeline[index];
+		for (let j = 0; j < beatInfo.noOfBeats; j++) {
+			const p1Index = evolutionPairs[index][j].parent1;
+			const p2Index = evolutionPairs[index][j].parent2;
+			const parentIndices = [p1Index, p2Index];
+			const score1 = beatlist[p1Index].score;
+			const score2 = beatlist[p2Index].score;
+
+			const strokeWidth = [mapRange(score1, 1, 5, 1, 5), mapRange(score2, 1, 5, 1, 5)];
+			// Add weak and strong classname to intensify the opacity
+			const lineClass = cx('line', {weak: (score1 + score2) <= 5, strong: (score1 + score2) >= 9});						
+			
+			// Add two lines per domNode
+			for(let lineIndex = 0; lineIndex < 2; lineIndex++) {
+				lines.push(<path
+					id={'line' + index + j + lineIndex}
+					stroke={colors[parentIndices[lineIndex]]}
+					strokeWidth={strokeWidth[lineIndex]}
+					className={lineClass}
+					key={'line' + index + j + lineIndex}
+					fill="transparent"
+				/>);
+			}
+		}
+		return lines;
+	}
 
 	// Doesn't work yet. Should add a glow-effect to the lines.
-	addFilter = () => {
+	addFilter() {
 		const svg = document.getElementById('svg');
 		if (svg) {
 			const defs = svg.append('defs');
@@ -114,63 +144,77 @@ class Lines extends React.Component {
 		this.setState({linesTimeline: []});
 	}
 
-	/* Calculate the x, y-coordinates for the lines and draw them out */
-	renderLines(props) {
-		console.log("Render lines")
-		const { evolutionPairs, beatInfo, domNodesTimeline, timelineLength } = props;
-		// Skip one index, because the first generation does not need lines
-		for(let index = 0; index < timelineLength-1; index++) {
-			for (let i = 0; i < beatInfo.noOfBeats; i++) {
-				const parent1 = evolutionPairs[index][i].parent1;
-				const parent2 = evolutionPairs[index][i].parent2;
-		
-				const el1 = domNodesTimeline[index][parent1];
-				const el2 = domNodesTimeline[index][parent2];
-				const el3 = domNodesTimeline[index+1][i];
-				//console.log(domNodesTimeline)
-				// Start the bezier curves from the child nodes
-				const parent1Coord = getCenterCoords(el1);
-				const parent2Coord = getCenterCoords(el2);
-				const startCoord = getCenterCoords(el3);
-				
-				//console.log(startCoord)
-				// Control points. c = control, S = start, P1,P2 = parent 1, 2.
-				const cSx = startCoord.x + 50;
-				const cSy = startCoord.y - 250;
-				const cS2x = startCoord.x - 50;
-				const cP1x = parent1Coord.x + 50;
-				const cP1y = parent1Coord.y + 250;
-				const cP2x = parent2Coord.x - 50;
-				const cP2y = parent2Coord.y + 250;
-				//const sx1 = 1; const sx2 = 1; const sy1 = 1; const sy2 = 1;
-				
-				// Bezier curve coords
-				const d = 'M' + startCoord.x +' '+startCoord.y + ' C ' + cS2x +' '+ cSy + ', ' + cP1x +' '+ cP1y + ', ' + parent1Coord.x +' '+ parent1Coord.y;
-				const e = 'M' + startCoord.x +' '+startCoord.y + ' C ' + cSx +' '+ cSy + ', ' + cP2x +' '+ cP2y + ', ' + parent2Coord.x +' '+ parent2Coord.y;
-				
-				let lineDom1 = document.getElementById('line' + index + i + 0);
-				let lineDom2 = document.getElementById('line' + index + i + 1);
-				
-				if(lineDom1) {
-					lineDom1.setAttribute('d', d);
-					lineDom2.setAttribute('d', e);
-				}
-
-				const colors = ['#5C429B', '#81DFEF', '#1D2DBF', '#6D0F3A', '#FFFFFF', '#8FDD76', '#F286B1', '#EDDA54'];
-			}
+	// Render all the lines upon mounting
+	renderAllLines(props) {
+		for(let index = 0; index < props.timelineLength-1; index++) {
+			console.log(index)
+			this.renderLines(props, index);
 		}
 	}
+	/* Calculate the x, y-coordinates for the lines and draw them out */
+	renderLines(props, index) {
+		console.log("Render lines")
+		const { evolutionPairs, beatInfo, domNodesTimeline, timelineLength } = props;
+		
+		for (let i = 0; i < beatInfo.noOfBeats; i++) {
+			let coords = this.getCoords(props, index, i);
+			let bezVal = this.getBezierValues(coords);
+			
+			// Start the lines from the child nodes
+			const d = 'M' + coords.startCoord.x +' '+coords.startCoord.y + ' C ' + bezVal.cS2x +' '+ bezVal.cSy + ', ' + bezVal.cP1x +' '+ bezVal.cP1y + ', ' + coords.parent1Coord.x +' '+ coords.parent1Coord.y;
+			const e = 'M' + coords.startCoord.x +' '+coords.startCoord.y + ' C ' + bezVal.cSx +' '+ bezVal.cSy + ', ' + bezVal.cP2x +' '+ bezVal.cP2y + ', ' + coords.parent2Coord.x +' '+ coords.parent2Coord.y;
+			
+			let lineDom1 = document.getElementById('line' + index + i + 0);
+			let lineDom2 = document.getElementById('line' + index + i + 1);
+			
+			if(lineDom1) {
+				lineDom1.setAttribute('d', d);
+				lineDom2.setAttribute('d', e);
+			}
 
-  render() {
-	// The height of the whole svg. 40vh per beatlist. Should cover all beatlists.
-	const height = (this.props.timelineLength*40) + 'vh';
-    return (
-		<div className={s.root}>
-			<svg id="svg" style={{ height: 'calc('+height+' + 106px)'}}>
-				{ this.state.linesTimeline }
-			</svg>
-		</div>
-    );
+			const colors = ['#5C429B', '#81DFEF', '#1D2DBF', '#6D0F3A', '#FFFFFF', '#8FDD76', '#F286B1', '#EDDA54'];
+		}
+	}
+	// Get coordinates for the child and parent domnodes
+	getCoords(props, index, beatIndex) {
+		const { evolutionPairs, domNodesTimeline } = props;
+		let coords = {};
+		console.log(evolutionPairs[index])
+		const parent1 = evolutionPairs[index][beatIndex].parent1;
+		const parent2 = evolutionPairs[index][beatIndex].parent2;
+
+		const el1 = domNodesTimeline[index][parent1];
+		const el2 = domNodesTimeline[index][parent2];
+		const el3 = domNodesTimeline[index+1][beatIndex];
+		
+		coords.parent1Coord = getCenterCoords(el1);
+		coords.parent2Coord = getCenterCoords(el2);
+		coords.startCoord = getCenterCoords(el3);
+		return coords;
+	}
+	// Add bezier values for curvature to the coords
+	getBezierValues(coords) {
+		let bezierValues = {};
+		bezierValues.cSx = coords.startCoord.x + 50;
+		bezierValues.cSy = coords.startCoord.y - 250;
+		bezierValues.cS2x = coords.startCoord.x - 50;
+		bezierValues.cP1x = coords.parent1Coord.x + 50;
+		bezierValues.cP1y = coords.parent1Coord.y + 250;
+		bezierValues.cP2x = coords.parent2Coord.x - 50;
+		bezierValues.cP2y = coords.parent2Coord.y + 250;
+		return bezierValues;
+	}
+
+	render() {
+		// The height of the whole svg. 40vh per beatlist. Should cover all beatlists.
+		const height = (this.props.timelineLength*40) + 'vh';
+		return (
+			<div className={s.root}>
+				<svg id="svg" style={{ height: 'calc('+height+' + 106px)'}}>
+					{ this.state.linesTimeline }
+				</svg>
+			</div>
+		);
   }
 }
 
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -27,9 +27,10 @@ export function calculateLoginTime() {
 export function getCenterCoords(el) {
   const coords = {};
   const bcr = el.getBoundingClientRect();
-  // coords.x = el.offsetLeft + (el.offsetWidth / 2);
-  // coords.y = el.offsetTop + (el.offsetHeight / 2);
   coords.x = bcr.x + (bcr.width / 2);
   coords.y = bcr.y + (bcr.height / 2);
+  if(coords.y < 0) {
+    coords.y + 200;
+  }
   return coords;
 };
